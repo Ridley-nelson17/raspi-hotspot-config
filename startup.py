ec7412de5033622f4f56d4ca9c8440fb0b2657e4
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-12-05 10:15:44 -0600

    Update startup.py

--- startup.py
+++ startup.py
@@ -12,33 +12,37 @@ import requests
 from flask import Flask, request, send_from_directory, jsonify, render_template, redirect
 app = Flask(__name__, static_url_path='')
 
-currentdir = os.path.dirname(os.path.abspath(__file__))
+
+currentdir = os.path.dirname(os.path.abspath(__file__)) # get our current working directory
 os.chdir(currentdir)
 
-ssid_list = []
+ssid_list = [] # create the list of wifi networks
+
 def getssid():
-    global ssid_list
-    if len(ssid_list) > 0:
-        return ssid_list
+    global ssid_list # make the list variable global
+    if len(ssid_list) > 0: return ssid_list 
     ssid_list = []
     get_ssid_list = subprocess.check_output(('iw', 'dev', 'wlan0', 'scan', 'ap-force'))
-    ssids = get_ssid_list.splitlines()
+    
+    # for every wifi name, add it to our list
+    ssids = get_ssid_list.splitlines() 
     for s in ssids:
         s = s.strip().decode('utf-8')
         if s.startswith("SSID"):
             a = s.split(": ")
-            try:
-                ssid_list.append(a[1])
-            except:
-                pass
+            try: ssid_list.append(a[1])
+            except: pass
     print(ssid_list)
+    ssid_list.append("join hotspot without choosing a network")
     ssid_list = sorted(list(set(ssid_list)))
     return ssid_list
 
-def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+
+def id_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    """Generates a unique ID"""
     return ''.join(random.choice(chars) for _ in range(size))
 
-wpa_conf = """country=GB
+wpa_conf = """country=US
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 network={
@@ -46,31 +50,32 @@ network={
     %s
 }"""
 
-wpa_conf_default = """country=GB
+wpa_conf_default = """country=US
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 """
 
-
-
 @app.route('/')
 def main():
+    """Opens index.html"""
     piid = open('pi.id', 'r').read().strip()
-    return render_template('index.html', ssids=getssid(), message="Once connected you'll find IP address @ <a href='https://snaptext.live/{}' target='_blank'>snaptext.live/{}</a>.".format(piid,piid))
+    return render_template('index.html', ssids=getssid(), message="Once connected please go to: <a href='https://snaptext.live/{}' target='_blank'>snaptext.live/{}</a> to find the router's IP address.".format(piid,piid))
 
 # Captive portal when connected with iOS or Android
 @app.route('/generate_204')
 def redirect204():
-    return redirect("http://192.168.4.1", code=302)
+    """Redirects Devices"""
+    return redirect("http://210.17.04.204", code=302) # used to be 192.168.4.1
 
 @app.route('/hotspot-detect.html')
 def applecaptive():
-    return redirect("http://192.168.4.1", code=302)
+    """Redirects Apple Devices"""
+    return redirect("http://210.17.04.204", code=302)
 
 # Not working for Windows, needs work!
 @app.route('/ncsi.txt')
 def windowscaptive():
-    return redirect("http://192.168.4.1", code=302)
+    return redirect("http://210.17.04.204", code=302)
 
 def check_cred(ssid, password):
     '''Validates ssid and password and returns True if valid and False if not valid'''
@@ -79,12 +84,9 @@ def check_cred(ssid, password):
     wpalog = wpadir + 'wpa.log'
     wpapid = wpadir + 'wpa.pid'
 
-    if not os.path.exists(wpadir):
-        os.mkdir(wpadir)
+    if not os.path.exists(wpadir): os.mkdir(wpadir) # if the wpa directory doesnt exist, make it
 
-    for _file in [testconf, wpalog, wpapid]:
-        if os.path.exists(_file):
-            os.remove(_file)
+    for _file in [testconf, wpalog, wpapid]: if os.path.exists(_file): os.remove(_file) # remove old files
 
     # Generate temp wpa.conf
     result = subprocess.check_output(['wpa_passphrase', ssid, password])
@@ -92,9 +94,7 @@ def check_cred(ssid, password):
         f.write(result.decode('utf-8'))
 
     def stop_ap(stop):
-        if stop:
-            # Services need to be stopped to free up wlan0 interface
-            print(subprocess.check_output(['systemctl', "stop", "hostapd", "dnsmasq", "dhcpcd"]))
+        if stop: print(subprocess.check_output(['systemctl', "stop", "hostapd", "dnsmasq", "dhcpcd"])) # Services need to be stopped to free up wlan0 interface
         else:
             print(subprocess.check_output(['systemctl', "restart", "dnsmasq", "dhcpcd"]))
             time.sleep(15)
@@ -106,15 +106,9 @@ def check_cred(ssid, password):
 
     stop_ap(True)
 
-    result = subprocess.check_output(['wpa_supplicant',
-                                      "-Dnl80211",
-                                      "-iwlan0",
-                                      "-c/" + testconf,
-                                      "-f", wpalog,
-                                      "-B",
-                                      "-P", wpapid])
+    result = subprocess.check_output(['wpa_supplicant', "-Dnl80211", "-iwlan0", "-c/" + testconf, "-f", wpalog, "-B", "-P", wpapid])
 
-    checkwpa = True
+    checkwpa = True # set our "check for wifi"
     while checkwpa:
         with open(wpalog, 'r') as f:
             content = f.read()
@@ -138,31 +132,31 @@ def check_cred(ssid, password):
 
 @app.route('/static/<path:path>')
 def send_static(path):
+    """retrieves static files"""
     return send_from_directory('static', path)
 
 @app.route('/signin', methods=['POST'])
 def signin():
     email = request.form['email']
+    if request.form['ssid'] == "join hotspot without choosing a network":
+        password = ""
+        
     ssid = request.form['ssid']
     password = request.form['password']
 
     pwd = 'psk="' + password + '"'
-    if password == "":
-        pwd = "key_mgmt=NONE" # If open AP
+    if password == "": pwd = "key_mgmt=NONE" # If open AP
 
     print(email, ssid, password)
     valid_psk = check_cred(ssid, password)
     if not valid_psk:
-        # User will not see this because they will be disconnected but we need to break here anyway
-        return render_template('ap.html', message="Wrong password!")
+        return render_template('ap.html', message="Incorrect password please try again") # User will not see this because they will be disconnected but we need to break here anyway
 
-    with open('wpa.conf', 'w') as f:
-        f.write(wpa_conf % (ssid, pwd))
-    with open('status.json', 'w') as f:
-        f.write(json.dumps({'status':'disconnected'}))
+    with open('wpa.conf', 'w') as f: f.write(wpa_conf % (ssid, pwd))
+    with open('status.json', 'w') as f: f.write(json.dumps({'status':'disconnected'}))
     subprocess.Popen(["./disable_ap.sh"])
     piid = open('pi.id', 'r').read().strip()
-    return render_template('index.html', message="Please wait 2 minutes to connect. Then your IP address will show up at <a href='https://snaptext.live/{}'>snaptext.live/{}</a>.".format(piid,piid))
+    return render_template('index.html', message="Please wait a few seconds to connect. Then your IP address will show up at <a href='https://snaptext.live/{}'>snaptext.live/{}</a>.".format(piid,piid))
 
 def wificonnected():
     result = subprocess.check_output(['iwconfig', 'wlan0'])
@@ -175,48 +169,42 @@ def wificonnected():
 if __name__ == "__main__":
     # things to run the first time it boots
     if not os.path.isfile('pi.id'):
-        with open('pi.id', 'w') as f:
-            f.write(id_generator())
+        with open('pi.id', 'w') as f: f.write(id_generator())
         subprocess.Popen("./expand_filesystem.sh")
         time.sleep(300)
     piid = open('pi.id', 'r').read().strip()
     print(piid)
     time.sleep(15)
-    # get status
-    s = {'status':'disconnected'}
-    if not os.path.isfile('status.json'):
-        with open('status.json', 'w') as f:
-            f.write(json.dumps(s))
-    else:
-        s = json.load(open('status.json'))
-
+    
+    status = {'status':'disconnected'} # get status
+    if not os.path.isfile('status.json'): 
+        with open('status.json', 'w') as f: f.write(json.dumps(status))
+    else: 
+        status = json.load(open('status.json'))
     # check connection
     if wificonnected():
-        s['status'] = 'connected'
+        status['status'] = 'connected'
     if not wificonnected():
-        if s['status'] == 'connected': # Don't change if status in status.json is hostapd
-            s['status'] = 'disconnected'
+        if status['status'] == 'connected': # Don't change if status in status.json is hostapd
+            status['status'] = 'disconnected'
 
-    with open('status.json', 'w') as f:
-        f.write(json.dumps(s))
-    if s['status'] == 'disconnected':
-        s['status'] = 'hostapd'
-        with open('status.json', 'w') as f:
-            f.write(json.dumps(s))
-        with open('wpa.conf', 'w') as f:
-            f.write(wpa_conf_default)
+    with open('status.json', 'w') as f: f.write(json.dumps(status))
+    if status['status'] == 'disconnected':
+        status['status'] = 'hostapd'
+        with open('status.json', 'w') as f: f.write(json.dumps(status))
+        with open('wpa.conf', 'w') as f: f.write(wpa_conf_default)
         subprocess.Popen("./enable_ap.sh")
-    elif s['status'] == 'connected':
+    elif status['status'] == 'connected':
         piid = open('pi.id', 'r').read().strip()
 
         # get ip address
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
+        status = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        status.connect(("8.8.8.8", 80))
         ipaddress = s.getsockname()[0]
-        s.close()
+        status.close()
 
         # alert user on snaptext
-        r = requests.post("https://snaptext.live",data=json.dumps({"message":"Your Pi is online at {}".format(ipaddress),"to":piid,"from":"Raspberry Pi Turnkey"}))
+        r = requests.post("https://snaptext.live",data=json.dumps({"message":"Your router is online at {}".format(ipaddress),"to":piid,"from":"liOS Cloud Computing Services"}))
         print(r.json())
         subprocess.Popen("./startup.sh")
         while True:
